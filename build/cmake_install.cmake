5268b2f2d68d86e400d4c4ca7ad628ede122d8dc
benjjiu04 <benjaminvargas2004@hotmail.com>
2026-08-17 20:42:04 -0300

    .

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/eduar/Documentos/c-c++/libzip_c
+# Install script for directory: C:/Users/eduar/Documentos/c-c++/zip_cli
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/eduar/Documentos/c-c++/libzip_c/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/eduar/Documentos/c-c++/zip_cli/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/eduar/Documentos/c-c++/libzip_c/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/eduar/Documentos/c-c++/zip_cli/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
